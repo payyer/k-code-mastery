49abad5891229608f308f3683f81cafa0b76c8cf
payyer <quocanhle112@gmail.com>
2024-08-12 16:50:55 +0700

    Init redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '../features/counter/counterSlice';
+
 export default function Home() {
-  return <div>Home page</div>;
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
