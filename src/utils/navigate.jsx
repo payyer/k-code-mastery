5d08ecf8a029e28ae3439f8b4a6061f2ac241be0
payyer <quocanhle12@gmail.com>
2024-08-15 12:50:37 +0700

    feature/ course detail page

--- src/utils/navigate.jsx
+++ src/utils/navigate.jsx
@@ -1,11 +1,11 @@
-import CrouseDetail from '../pages/CourseDetailPage/CrouseDetail';
-import Home from '../pages/Home';
-import Lesson from '../pages/Lesson';
+import CrouseDetail from "../pages/CourseDetailPage/CrouseDetail";
+import Home from "../pages/Home";
+import Lesson from "../pages/Lesson";
 
 const linkPage = [
-  { link: '/', element: <Home /> },
-  { link: '/course/:id', element: <CrouseDetail /> },
-  { link: '/lesson/:id', element: <Lesson /> },
+  { link: "/", element: <Home /> },
+  { link: "/course/:id", element: <CrouseDetail /> },
+  { link: "/lesson/:id", element: <Lesson /> },
 ];
 
 export default linkPage;
